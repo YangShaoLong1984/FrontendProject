10e8c144014c6e516109a7415f343c30fb063838
yangsl <1528945226@qq.com>
2022-07-21 23:06:31 +0800

    net

--- Interviewer/手写代码/test.js
+++ Interviewer/手写代码/test.js
@@ -1,15 +1,19 @@
-function myInstanceof(left, right) {
-    let proto = Object.getPrototypeOf(left);
-    let prototype = right.prototype;
-    while (true) {
-        if (proto === null) {
-            return false;
-        }
-        if (proto === prototype) {
-            return true;
-        }
-        proto = Object.getPrototypeOf(proto);
-    }   
-}
+Promise.resolve().then(() => {
+    console.log(0)
+    return Promise.resolve(4)
+}).then(res => {
+    console.log(res)
+})
 
-console.log(myInstanceof({}, Array));
+Promise.resolve().then(() => {
+    console.log(1)
+}).then(() => {
+    console.log(2)
+}).then(() => {
+    console.log(3)
+}).then(() => {
+    console.log(5)
+}).then(() => {
+    console.log(6)
+})
+//  0 1 2 3 4 5 6
