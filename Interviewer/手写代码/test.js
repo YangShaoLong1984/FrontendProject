c04b24aa1d5412fcd46015b6d1438420eef651da
yangsl <1528945226@qq.com>
2022-07-22 16:23:30 +0800

    str

--- Interviewer/手写代码/test.js
+++ Interviewer/手写代码/test.js
@@ -1,19 +1,10 @@
-Promise.resolve().then(() => {
-    console.log(0)
-    return Promise.resolve(4)
-}).then(res => {
-    console.log(res)
-})
-
-Promise.resolve().then(() => {
-    console.log(1)
-}).then(() => {
-    console.log(2)
-}).then(() => {
-    console.log(3)
-}).then(() => {
-    console.log(5)
-}).then(() => {
-    console.log(6)
-})
-//  0 1 2 3 4 5 6
+let s = "We are happy."
+let arr = s.split('');
+console.log(arr);
+for (let a of arr) {
+    // console.log(a);
+    if (a === ' ') {
+        a = '2'
+    }
+}
+console.log(arr);
